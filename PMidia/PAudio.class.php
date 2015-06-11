af1907071c7ba61c5fe1b1b31ce139492f3ba2b7
ale-php <alexandre@progs.net.br>
2015-06-11 18:15:15 -0300

    Atualizado para 2.0

---
+++ PMidia/PAudio.class.php
@@ -0,0 +1,50 @@
+<?php
+class PAudio extends TElement{
+
+ protected $sources;
+ protected  $content;
+
+
+    public function __construct()
+    {
+        parent::__construct('audio');
+
+
+        $this->id = 'audio'.uniqid();
+       
+        $this->controls = true;
+        
+        $this->sources = array();
+        
+}
+
+/**
+* camonho do video
+*@param $source /audio/audio.mp3
+*formato do video
+*@param $type video/mp4 , video/ogg
+*/
+public function addSource($source,$type){
+
+$audio = new TElement('source');
+$audio->src = $source;
+$audio->type = $type;
+
+$this->sources[] = $audio;
+
+}
+
+
+public function show(){
+
+foreach($this->sources as $s):
+
+parent::add($s);
+
+endforeach;
+
+parent::show();
+
+}
+}
+?>
---
+++ PMidia/PVideo.class.php
@@ -0,0 +1,51 @@
+<?php
+class PVideo extends TElement{
+
+ protected $sources;
+ protected  $content;
+
+
+    public function __construct($width,$heigth)
+    {
+        parent::__construct('video');
+
+
+        $this->id = 'video'.uniqid();
+        $this->width = $width;
+        $this->heigth = $heigth;
+        $this->controls = true;
+        
+        $this->sources = array();
+        
+}
+
+/**
+* camonho do video
+*@param $source /video/video.mp4
+*formato do video
+*@param $type video/mp4 , video/ogg
+*/
+public function addSource($source,$type){
+
+$video = new TElement('source');
+$video->src = $source;
+$video->type = $type;
+
+$this->sources[] = $video;
+
+}
+
+
+public function show(){
+
+foreach($this->sources as $s):
+
+parent::add($s);
+
+endforeach;
+
+parent::show();
+
+}
+}
+?>
