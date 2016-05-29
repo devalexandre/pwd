87e4586d420a191f4856d80adb1a83d54d29349c
Alexandre <alexandre@Alice.domain.name>
2016-05-28 23:51:20 -0300

    adcionado converter html para pdf

---
+++ PTableWrite/PTablePDF.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandre
+ * Date: 10/25/14
+ * Time: 2:24 AM
+ */
+
+use Dompdf\Dompdf;
+
+class PTablePDF {
+   
+    private $pdf;
+
+
+
+    /**
+     * Método construtor
+     * @param $widths vetor contendo as larguras das colunas
+     */
+    public function __construct()
+    {
+
+
+
+ }
+ 
+ /**
+     * Armazena o conteúdo do documento em um arquivo
+     * @param $filename caminho para o arquivo de saída
+     */
+    public function save($file,$filename)
+    {
+    
+    try{
+   // define('DOMPDF_ENABLE_AUTOLOAD', true);
+    
+
+require_once 'dompdf/autoload.inc.php';
+
+$this->pdf =  new Dompdf();
+
+    $contents = file_get_contents($file);
+    
+
+  $this->pdf->load_html($contents);
+ //$this->pdf->set_paper('letter', 'portraite');
+$this->pdf->render();
+
+ file_put_contents("app/reports/{$filename}.pdf", $this->pdf->output());
+   
+    }catch(Exception $e){
+    
+    echo $e->getMessage();
+    }
+    }
+
+}
