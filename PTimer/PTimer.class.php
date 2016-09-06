82454ed92ae4342cae256966675b6c837efd29ea
ale-php <alexandre@progs.net.br>
2016-09-06 19:44:48 -0300

    PTimer atualizado para usar mais de um

--- PTimer/PTimer.class.php
+++ PTimer/PTimer.class.php
@@ -1,89 +1,91 @@
 <?php
 
 /**
- * PCepProgs 
+ * PCepProgs
  *
- * @version    1.0
- * @version adianti framework 2.0
+ * @version    2.0
+ * @version adianti framework 3.0
  * @package    PTimer
  * @author     Alexandre E. Souza
- 
+
  */
- 
+
 
 class PTimer {
 
-private $banco;
-private $model;
-private $action;
-private $time;
-private $campos;
-private $key;
+    private $banco;
+    private $model;
+    private $action;
+    private $time;
+    private $campos;
+    private $key;
+    private $file;
 
-/**
+    /**
 @param $banco banco a ser usado
 @param  $model model a ser usada
 @param  $campos  campos a serem exibidos
 @param  $action type array (controller,action)
-@param $key  key da action 
+@param $key  key da action
 @param  $time tempo para reexecutar o timer
 
 */
-function __construct($banco,$model,$campos,$action,$key,$time){
+    function __construct($banco,$model,$campos,$action,$key,$time,$file){
 
 
-$this->banco = $banco;
-$this->model = $model;
-$this->action = $action;
-$this->time = $time;
-$this->campos = $campos;
-$this->key = $key;
+        $this->banco = $banco;
+        $this->model = $model;
+        $this->action = $action;
+        $this->time = $time;
+        $this->campos = $campos;
+        $this->key = $key;
+        $this->file = $file;
 
-}
-
-
-
+    }
 
 
-public function show(){
 
 
-$time = new TElement('div');
 
-$time->id = "ptimer_".uniqid();
-
-$campos = serialize($this->campos);
-$action = serialize($this->action);//$campos = base64_encode($campos);
+    public function show(){
 
 
-if(!file_exists('timer.php')){
+        $time = new TElement('div');
 
-$file = file_get_contents('app/lib/PWD-Lib/PTimer/timer.php');
-$fp = fopen("timer.php", "a");
- 
+        $time->id = "ptimer_".uniqid();
 
-$escreve = fwrite($fp, "$file");
- 
-// Fecha o arquivo
-fclose($fp);
-}
-$url = "'timer.php'";
+        $campos = serialize($this->campos);
+        $action = serialize($this->action);//$campos = base64_encode($campos);
 
 
-$code = "
+        if(!file_exists($this->file.'.php')){
+
+            $file = file_get_contents('app/lib/PWD-Lib/PTimer/'.$this->file.'.php');
+            $fp = fopen($this->file.".php", "a");
+
+
+            $escreve = fwrite($fp, "$file");
+
+            // Fecha o arquivo
+            fclose($fp);
+        }
+        $url = "'"$this->file.".php'";
+
+
+        $code = "
 
 runPtimer($url,'$this->model','$this->banco','$campos','$action','$this->key',$this->time,'$time->id');
 
 ";
 
- TScript::create($code);
- 
+        TScript::create($code);
 
 
 
-$time->show();
 
-}
+        $time->show();
+
+    }
 
 
 
